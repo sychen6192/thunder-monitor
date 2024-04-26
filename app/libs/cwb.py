a996cdafa2bfa2aab60834ef4b371103442eee10
Jack SY Chen <qazxsw9295@gmail.com>
2024-04-26 23:55:43 +0800

    [feat] update ep

--- app/libs/cwb.py
+++ app/libs/cwb.py
@@ -9,7 +9,7 @@ from app.libs.utils import read_config
 
 def download_thunder_img(output):
     try:
-        res = requests.get("https://www.cwb.gov.tw/Data/lightning/lightning_s.jpg")
+        res = requests.get("https://www.cwa.gov.tw/Data/lightning/lightning_s.jpg")
         if res.status_code == 200:
             img = Image.open(BytesIO(res.content))
             crop_img = img.crop((415, 520, 510, 630))
@@ -21,7 +21,7 @@ def download_thunder_img(output):
         raise Exception(err)
 
 def get_thunder_data(token):
-    resp = urlopen(f"https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/O-A0039-001?Authorization={token}&downloadType=WEB&format=KMZ")
+    resp = urlopen(f"https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0039-001?Authorization={token}&downloadType=WEB&format=KMZ")
     kmz = ZipFile(BytesIO(resp.read()))
     kml = kmz.open('doc.kml', 'r').read()
     doc = html.fromstring(kml)
