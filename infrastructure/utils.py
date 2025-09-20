58dd3497ecba7025c5690e7701a5d7bfb48de8e4
Jack SY Chen <qazxsw9295@gmail.com>
2025-09-20 18:11:27 +0800

    fix: timezone

--- infrastructure/utils.py
+++ infrastructure/utils.py
@@ -1,9 +1,12 @@
-import datetime
+from datetime import datetime
+from zoneinfo import ZoneInfo 
 
 def get_google_url(long, lat):
     return f"https://www.google.com/maps?q={long},{lat}"
 
-def diff_time(occur_time):
-    now = datetime.datetime.now()
-    start = datetime.datetime.strptime(occur_time, "%Y-%m-%d %H:%M")
+def diff_time(occur_time: str) -> int:
+    tw_zone = ZoneInfo("Asia/Taipei")
+    now = datetime.now(tz=tw_zone)
+    start = datetime.strptime(occur_time, '%Y-%m-%d %H:%M')
+    start = start.replace(tzinfo=tw_zone)
     return int((now - start).total_seconds())
