40023c46bd078c28b4d08c969fb8e363643748da
Jack SY Chen <qazxsw9295@gmail.com>
2025-09-20 17:10:07 +0800

    add: argParse to seperate env

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 from services.alert_service import AlertService
 from infrastructure.config import load_config
 from loguru import logger
-
+import argparse
 
 def main() -> None:
     try:
-        config = load_config("STAGE")
+        config = load_config(env=parse_args())
         logger.add(config["LOG"], rotation="1 week")
         service = AlertService(config)
         service.run()
     except Exception as e:
         logger.exception(e)
 
+def parse_args() -> str:
+    parser = argparse.ArgumentParser(description="Thunder Monitor")
+    parser.add_argument("--env", default="PROD", choices=["PROD", "STAGE"], help="Environment config")
+    args = parser.parse_args()
+    return args.env
 
 if __name__ == "__main__":
     main()
